2bcf3e308788bc23819bc87787a38ad8ab61b0c2
yanyukiai <78672128+yanyukiai@users.noreply.github.com>
2023-12-18 11:30:36 -0500

    Add basic math functions in javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,41 @@
 console.log("Hello, Calculator Project!");
+
+console.log(operate("add", 15, 20));
+console.log(operate("substract", 15, 20));
+console.log(operate("multiply", 15, 2));
+console.log(operate("divide", 15, 2));
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "add":
+      return add(a, b);
+      break;
+    case "substract":
+      return substract(a, b);
+      break;
+    case "multiply":
+      return multiply(a, b);
+      break;
+    case "divide":
+      return divide(a, b);
+      break;
+    default:
+      return "Operator is not among add, substract, multiply or divide.";
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
