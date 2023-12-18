e55e1634211427c4e9d350145d511b0e49d4f558
yanyukiai <78672128+yanyukiai@users.noreply.github.com>
2023-12-18 13:07:22 -0500

    Edit .js file to display input history

--- javascript.js
+++ javascript.js
@@ -5,6 +5,77 @@ console.log(operate("substract", 15, 20));
 console.log(operate("multiply", 15, 2));
 console.log(operate("divide", 15, 2));
 
+/**
+ * display-prev window logic:
+ *
+ * User input: numbers, operators, del, AC, equal, TBD (ignore for now)
+ * display-prev results:
+ * 1. input AC: 0
+ * 2. input numbers/operators/TBD: display previous inputs (stored in variables)
+ * 3. input del:
+ *      If no prev input, display 0
+ *      If there's prev input, display previous inputs with last input deleted (stored in variables)
+ * 4. input equal:
+ *      check if the input is syntax correct:
+ *          correct: display results
+ *          wrong: display syntax error
+ * */
+
+let storedInput = "";
+let currentInput = "";
+
+const displayCurrent = document.querySelector(".display-current");
+const displayPrev = document.querySelector(".display-prev");
+
+const btnNumbers = document.querySelectorAll(".numbers");
+btnNumbers.forEach((button) => {
+  button.addEventListener("click", () => {
+    currentInput += button.id;
+    displayCurrent.textContent = currentInput;
+    storedInput += button.id;
+    displayPrev.textContent = storedInput;
+  });
+});
+
+const btnOperators = document.querySelectorAll(".operators");
+btnOperators.forEach((button) => {
+  button.addEventListener("click", () => {
+    displayCurrent.textContent = button.id;
+    currentInput = "";
+    storedInput += button.id;
+    displayPrev.textContent = storedInput;
+  });
+});
+
+const btnDel = document.querySelector(".del");
+btnDel.addEventListener("click", () => {
+  displayCurrent.textContent = "0";
+});
+
+const btnAC = document.querySelector(".AC");
+btnAC.addEventListener("click", () => {
+  displayCurrent.textContent = "0";
+  displayPrev.textContent = "0";
+  storedInput = "";
+  currentInput = "";
+});
+
+const btnEqual = document.querySelector(".equal");
+btnEqual.addEventListener(
+  "click",
+  () => (displayCurrent.textContent = "TBD: calculation results")
+);
+
+const btnTBDs = document.querySelectorAll(".TBD");
+btnTBDs.forEach((button) => {
+  button.addEventListener("click", () => {
+    displayCurrent.textContent = button.id;
+    displayPrev.textContent = button.id;
+    storedInput = "TBD";
+    currentInput = "TBD";
+  });
+});
+
 function operate(operator, a, b) {
   switch (operator) {
     case "add":
@@ -39,45 +110,3 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
-
-const displayCurrent = document.querySelector(".display-current");
-
-const btnNumbers = document.querySelectorAll(".numbers");
-btnNumbers.forEach((button) => {
-  button.addEventListener(
-    "click",
-    () => (displayCurrent.textContent = button.id)
-  );
-});
-
-const btnOperators = document.querySelectorAll(".operators");
-btnOperators.forEach((button) => {
-  button.addEventListener(
-    "click",
-    () => (displayCurrent.textContent = button.id)
-  );
-});
-
-const btnDel = document.querySelector(".del");
-btnDel.addEventListener("click", () => {
-  displayCurrent.textContent = "0";
-});
-
-const btnAC = document.querySelector(".AC");
-btnAC.addEventListener("click", () => {
-  displayCurrent.textContent = "0";
-});
-
-const btnTBDs = document.querySelectorAll(".TBD");
-btnTBDs.forEach((button) => {
-  button.addEventListener(
-    "click",
-    () => (displayCurrent.textContent = button.id)
-  );
-});
-
-const btnEqual = document.querySelector(".equal");
-btnEqual.addEventListener(
-  "click",
-  () => (displayCurrent.textContent = "TBD: calculation results")
-);
